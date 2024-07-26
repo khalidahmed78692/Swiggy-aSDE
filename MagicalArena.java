dee599ef3f852c58688386c3eab8de2b99650ecb
Khalid <khalidahmed78692@gmail.com>
2024-07-27 00:11:27 +0530

    initialize required variables and create a constructor

--- MagicalArena.java
+++ MagicalArena.java
@@ -1,3 +1,9 @@
 public class MagicalArena {
-    
+    private int health, attribute, strength;
+
+    MagicalArena(int health, int attribute, int strength) {
+        this.health = health;
+        this.attribute = attribute;
+        this.strength = strength;
+    }
 }
